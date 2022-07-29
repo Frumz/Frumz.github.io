fd6888118c9b05eda4a3fc186adad2dd3b1ab15b
Frumz <web.nik.shep@gmail.com>
2022-07-29 23:08:41 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,66 @@
+const cards = document.querySelectorAll('.card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
+let cars = document.querySelector('.card');
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
+
+    secondCard = this;
+
+    checkForMatch();
+}
+
+function checkForMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disableCards()
+        
+        return;
+        
+    }
+    unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    firstCard.classList.add('hidden');
+    secondCard.classList.add('hidden');
+
+    resetBoard();
+}
+
+function unflipCards() {
+        lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
+        
+        resetBoard();
+    }, 900);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})()
+
+cards.forEach(card => card.addEventListener('click', flipCard));
